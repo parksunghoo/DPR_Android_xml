7a313c0985d1be5b5b139f7970a62408c44e545d
박성훈 <newinnobiz@gmail.com>
2022-06-14 14:17:10 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/dgist/dpr_android_xml/MainActivity.java
@@ -0,0 +1,35 @@
+package kr.ac.dgist.dpr_android_xml;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // allow the js
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); //화면이 계속 켜짐
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://210.104.190.225:42502/swagger/index.html?urls.primaryName=XML%20to%20DB%20API%20v1.0");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
